da4223c7c60b83ccfbf84f013305c92786fd066f
dvidsilva <contacto@dvidsilva.com>
2019-10-21 22:42:11 -0400

    empty commit to trigger version

--- packages/styles/src/index.js
+++ packages/styles/src/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    description: 'global and reset styles to use in other projects',
+    description: 'Global and reset styles to use in other projects',
     run: function run() {},
 };
