b6379f0b08ecb339a96a2e2a66d27583fe98a7ac
dvidsilva <contacto@dvidsilva.com>
2019-10-21 22:36:08 -0400

    Added more things to sample styles package

---
+++ packages/styles/__test__/src.index.test.js
@@ -0,0 +1,3 @@
+describe('Styles', () => {
+    test.todo('needs tests');
+});
---
+++ packages/styles/src/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    description: 'global and reset styles to use in other projects',
+    run: function() {},
+}
